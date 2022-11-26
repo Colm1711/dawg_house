326c3398f812ea38de3f2ed1c648cbbb007bd982
Colm1711 <cosullivan1711@gmail.com>
2022-11-26 17:35:55 +0000

    Add user profile display, search and filters to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    This class controls the Admin view of Users
+
+    Lists how the content is presetned to the Admin User and
+    provides List filter panel.
+
+    Search item fields:
+    'First Name'
+    'Last Name'
+    'Phone Number'
+    'Email'
+    'Is Service Provider'
+    """
+    list_display = ('first_name', 'last_name', 'phone_number',
+                    'is_service_provider')
+    search_fields = ['first_name', 'last_name', 'phone_number',
+                     'is_service_provider']
+    list_filter = ('first_name', 'last_name', 'phone_number',
+                   'is_service_provider')
