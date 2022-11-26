27c9225e5ae162d4ec531453acfbcc4d0e53784e
Colm1711 <cosullivan1711@gmail.com>
2022-11-26 22:49:33 +0000

    add logic to show user data and update

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UpdateUserForm, UpdateUserProfileForm
 
 
+@login_required
 def update_profile(request):
     """
     This function handles rendering of the update page to the user
@@ -8,4 +12,34 @@ def update_profile(request):
     User must be signed in to do this.
     """
     template = 'profiles/profile.html'
-    return render(request, template)
+    success_message = "Success"
+    error_message = "Fail"
+    userprofile = request.user.userprofile
+
+    if request.method == 'POST':
+        # post forms
+        user_update_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_update_form = UpdateUserProfileForm(request.POST,
+                                                    instance=userprofile)
+
+        # save new user data if forms are valid
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            return redirect('profile')
+        else:
+            # send message to user if something goes wrong and redirect
+            print(error_message)
+    else:
+        # get forms to show current data to user
+        user_update_form = UpdateUserForm(instance=request.user)
+        profile_update_form = UpdateUserProfileForm(
+                                                    instance=userprofile)
+
+        # pass forms to the html form
+        context = {
+                    'user_update_form': user_update_form,
+                    'profile_update_form': profile_update_form,
+                    }
+
+    return render(request, template, context)
