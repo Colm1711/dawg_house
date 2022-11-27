af5677013bde8ca7af2c43ff61daf3dc8fdb2428
Colm1711 <cosullivan1711@gmail.com>
2022-11-27 18:08:40 +0000

    add custom account adapter for service provider redirect

---
+++ profiles/adaptor.py
@@ -0,0 +1,21 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
+from django.shortcuts import resolve_url
+
+
+class AccountAdapter(DefaultAccountAdapter):
+    """
+    This class handles the log in rediect for when user is
+    a service provider or just a general user.
+
+    User redirected to settings.LOGIN_REDIRECT_URL = Home
+
+    Sends service provider to profile for DEVELOPMENT will update this.
+    """
+    def get_login_redirect_url(self, request):
+
+        if request.user.userprofile.is_service_provider is True:
+            url = '/profile'
+        else:
+            url = settings.LOGIN_REDIRECT_URL
+        return resolve_url(url)
