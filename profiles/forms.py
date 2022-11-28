58f2e17a8a7b2a61c3acc905892c7a39cc1c9c0f
Colm1711 <cosullivan1711@gmail.com>
2022-11-28 22:01:38 +0000

    add service provider form

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,7 +9,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 # Internal imports
-from .models import UserProfile
+from .models import UserProfile, ServiceProvider
 
 
 class RegisterForm(SignupForm):
@@ -82,3 +82,9 @@ class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['first_name', 'last_name', 'phone_number']
+
+
+class ServiceProviderForm(forms.ModelForm):
+    class Meta:
+        model = ServiceProvider
+        exclude = ("user", "slug", "created_on", "updated_on", "longitude", "latitude",)
