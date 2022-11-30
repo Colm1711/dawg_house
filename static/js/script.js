f101d872552effc205d730f8b6f2c72540e4c399
Colm1711 <cosullivan1711@gmail.com>
2022-11-30 21:17:28 +0000

    add javascript to auto dismiss messages

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+/* Function to close Booststrap alert after 3 seconds*/
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
