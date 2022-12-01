d2a3f08c16553534744b01a5bb3fb556d1aed105
Colm1711 <cosullivan1711@gmail.com>
2022-12-01 22:48:00 +0000

    add url path to the update service provider form

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.update_profile, name='profile'),
     path('delete_profile', views.delete_profile, name='delete_profile'),
     path('serviceprovider/', views.serviceprovider, name='serviceprovider'),
+    path('myservices/', views.update_serviceprovider, name='myservices'),
 ]
