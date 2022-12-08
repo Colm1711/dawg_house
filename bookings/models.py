21b643df11bad7196610d1f7194a5377b99b3505
Colm1711 <cosullivan1711@gmail.com>
2022-12-08 20:28:59 +0000

    Add Booking model to stor booking data

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# Imports
 
-# Create your models here.
+# Django imports
+from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import ServiceProvider, UserProfile
+
+
+class ServiceBooking(models.Model):
+    """
+    This is the class to store the data of the booking.
+    """
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_provider_id = models.ForeignKey(ServiceProvider,
+                                            on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    created_on = models.DateField(auto_now_add=True)
+    modified_on = models.DateField(auto_now=True)
+    service_provider_approved = models.BooleanField(default=False)
+    mod_is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user_id.email} | date: {self.date} | time: {self.time}"
