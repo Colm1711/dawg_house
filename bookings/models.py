0180bc0578c70cc47a6eec44ab4b03dce4c27adb
Colm1711 <cosullivan1711@gmail.com>
2022-12-08 20:29:58 +0000

    add booking number and stripe pid fields

--- bookings/models.py
+++ bookings/models.py
@@ -13,10 +13,14 @@ class ServiceBooking(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     service_provider_id = models.ForeignKey(ServiceProvider,
                                             on_delete=models.CASCADE)
+    booking_number = models.CharField(max_length=32, null=False,
+                                      editable=False)
     start_date = models.DateField()
     end_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     total = models.DecimalField(max_digits=6, decimal_places=2)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
+                                  default='')
     created_on = models.DateField(auto_now_add=True)
     modified_on = models.DateField(auto_now=True)
     service_provider_approved = models.BooleanField(default=False)
