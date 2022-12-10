47e0e75fd8eddf2c9a522ba1e5db79675075dffc
Colm1711 <cosullivan1711@gmail.com>
2022-12-10 21:43:18 +0000

    add stripe currency, public and secret key

--- dawghouse/settings.py
+++ dawghouse/settings.py
@@ -202,3 +202,9 @@ else:
     EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+# STRIPE
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
