ae7be102aeb577089c6e082b236caa5df0e054f2
Colm1711 <cosullivan1711@gmail.com>
2022-12-11 16:27:09 +0000

    add url for service provider list

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.booking_form, name='bookingform'),
+    path('', views.list_service_providers, name='booking'),
+    path('serviceproviders/', views.list_service_providers,
+         name='serviceproviderslist'),
+    path('bookingform/', views.booking_form, name='bookingform'),
 ]
