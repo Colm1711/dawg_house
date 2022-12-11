ff29a21e835c8c86312f5c65cc51c37712e40400
Colm1711 <cosullivan1711@gmail.com>
2022-12-11 23:24:31 +0000

    pass service provider pk to form

--- bookings/views.py
+++ bookings/views.py
@@ -2,14 +2,15 @@
 from datetime import datetime
 
 # Django imports
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.views import View
 
 # Internal imports
 from .forms import BookingForm
-from profiles.models import UserProfile
+from profiles.models import UserProfile, ServiceProvider
 
 
 def list_service_providers(request):
@@ -21,28 +22,33 @@ def list_service_providers(request):
                   {'serviceproviders': serviceproviders})
 
 
-@login_required
-def booking_form(request):
+@login_required(login_url='account/signup')
+def booking_form(request, pk):
     """
     This function handles rendering of the update page to the user
     and logic to update.
+
     User must be signed in to do this.
     """
 
     success_message = 'Your booking has been processed successfully'
     template = 'booking_form.html'
-    form = BookingForm(request.POST)
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
-
-    if form.is_valid():
-        form = form.save(commit=False)
-        form.user = request.user
-        form.price = form.service_provider_id.price_per_service
-        form.total = (get_total_num_of_days(form.start_date, form.end_date) *
-                      form.price)
-        form.save()
-        messages.success(request, success_message)
-        return redirect('/')
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.service_provider_id = ServiceProvider.objects.get(pk=pk)
+            form.price = form.service_provider_id.price_per_service
+            form.total = (get_total_num_of_days(form.start_date, form.end_date)
+                          * form.price)
+            form.save()
+            messages.success(request, success_message)
+            return redirect('/')
+    else:
+        form = BookingForm()
+        return render(request, template, {'form': form})
 
     # pass forms to the html form
     context = {'form': BookingForm,
