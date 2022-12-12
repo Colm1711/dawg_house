819449e7ab9186a4ec2a85d34aac1aba51ec042d
Colm1711 <cosullivan1711@gmail.com>
2022-12-12 16:27:26 +0000

    add contact urls

--- dawghouse/urls.py
+++ dawghouse/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('profile/', include('profiles.urls')),
     path('booking/', include('bookings.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
