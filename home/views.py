ec2b24eb88c39f0f03a9eeb8e16444fa2d381f7a
Colm1711 <cosullivan1711@gmail.com>
2022-12-12 18:55:23 +0000

    add about us view

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page"""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """A view to show the about page"""
+    return render(request, "home/about.html")
