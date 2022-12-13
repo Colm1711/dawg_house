2c9af4300545e692d83b421fb5ed45ad0d65d17e
Colm1711 <cosullivan1711@gmail.com>
2022-12-13 18:49:57 +0000

    add functionanlity to print bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,6 +12,16 @@ def bag_contents(request):
     bag_items = []
     total = 0
     service_count = 0
+    bag = request.session.get('bag', {})
+
+    for service_id, quantity in bag.items():
+        service = get_object_or_404(Service, pk=service_id)
+        service_count += quantity * service.cost
+        bag_items.append({
+            'service_id': service_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     context = {
         'bag_items': bag_items,
