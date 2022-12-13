f2f9833ddfc8440f532195bdb12d74b6f49e889f
Colm1711 <cosullivan1711@gmail.com>
2022-12-13 17:26:34 +0000

    add to bag function

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,10 @@
 # Imports
 
 # Djanog Imports
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# INternal imports
+from services.models import Service, Breed, Size
 
 
 def view_bag(request):
@@ -10,3 +13,23 @@ def view_bag(request):
     of bookings
     """
     return render(request, 'bag.html')
+
+
+def add_to_bag(request, service_id):
+    """
+    Add a service to the shopping cart
+    """
+    service = Service.objects.get(pk=service_id)
+    breed = request.POST['breed_selector']
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if service_id in list(bag.keys()):
+        bag['service_id'] = quantity
+    else:
+        bag[service_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
