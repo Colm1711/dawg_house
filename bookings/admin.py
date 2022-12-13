95f9081340c8c05ae0d84c3ecab7e796029d382b
Colm1711 <cosullivan1711@gmail.com>
2022-12-13 12:41:10 +0000

    removing app to refactor models and services

--- bookings/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import ServiceBooking
-
-admin.site.register(ServiceBooking)
--- bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_service_providers, name='booking'),
-    path('serviceproviders/', views.list_service_providers,
-         name='serviceproviderslist'),
-    path('bookingform/<int:pk>', views.booking_form, name='bookingform'),
-]
