a6513f5b92c2788f841300f39fee51082025c7e2
Colm1711 <cosullivan1711@gmail.com>
2022-12-13 16:16:25 +0000

    add breed and sizes to service detail view

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Service
+from .models import Service, Size, Breed
 
 
 def all_services(request):
@@ -18,9 +18,13 @@ def service_detail(request, service_id):
     """ A view to show individual service details """
 
     service = get_object_or_404(Service, pk=service_id)
+    breeds = Breed.objects.order_by('breed')
+    sizes = Size.objects.all()
 
     context = {
         'service': service,
+        'breeds': breeds,
+        'sizes': sizes,
     }
 
     return render(request, 'services/service_detail.html', context)
