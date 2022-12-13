6c8670e445a0f05b1811f976305bd7206504f26a
Colm1711 <cosullivan1711@gmail.com>
2022-12-13 12:45:27 +0000

    add services url

---
+++ services/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Service
+
+
+def all_services(request):
+    """ A view to show all services """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services.html', context)
