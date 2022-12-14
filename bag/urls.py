a80868815a6d86a136b922118cd9f158f5272834
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 01:25:13 +0000

    add url to delete service

--- bag/urls.py
+++ bag/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<service_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<service_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<service_id>', views.remove_from_bag,
+         name='remove_from_bag'),
 ]
