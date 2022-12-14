e761e20f9ef255833d122029c693462c53d396ac
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 03:45:48 +0000

    add models to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,62 @@
+# Imports
+
+# Django imports
 from django.contrib import admin
 
-# Register your models here.
+# Internal imports
+from .models import ServiceOrder, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Inline class for OrderLineItem
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+@admin.register(ServiceOrder)
+class ServiceOrderAdmin(admin.ModelAdmin):
+    """
+    A class for Service Order Admin
+
+    Lists how the content is presetned to the Admin User and
+    provides List filter panel.
+    """
+
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'created_on',
+        'order_total',
+        'original_bag',
+        'stripe_pid'
+    )
+
+    fields = (
+        'order_number',
+        'user',
+        'created_on',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'address_1',
+        'address_2',
+        'county',
+        'eircode',
+        'order_total',
+        'original_bag',
+        'stripe_pid'
+    )
+
+    list_display = (
+        'order_number',
+        'created_on',
+        'first_name',
+        'last_name',
+        'order_total',
+        )
+
+    ordering = ('-created_on',)
