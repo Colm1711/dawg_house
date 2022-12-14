4e5d6f1db6e7507b3a888b33a328b46971d10654
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 00:46:30 +0000

    add logic to adjust the bag contents

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 # Imports
 
 # Djanog Imports
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 # Internal imports
@@ -33,7 +33,7 @@ def add_to_bag(request, service_id):
                 messages.info(request,
                               (f'We have updated {quantity}\
                               {service.service_type} for your dog of choice\
-                              {breed}'))
+                              {breed} with another'))
             else:
                 bag[service_id]['items_by_breed'][breed] = quantity
                 messages.success(request, (f'We have added {quantity}\
@@ -41,9 +41,32 @@ def add_to_bag(request, service_id):
                                  choice {breed}'))
         else:
             bag[service_id] = {'items_by_breed': {breed: quantity}}
-            messages.error(request, 'Please pick a breed!')
+            messages.success(request, (f'We have added {quantity}\
+                              {service.service_type} for your dog of choice\
+                              {breed} with another'))
     else:
-        messages.error(request, 'You need to select your dogs breed!')
+        messages.warning(request, 'You need to select your dogs breed!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, service_id):
+    """ Adjust a service in the shopping cart bag """
+
+    service = Service.objects.get(pk=service_id)
+    quantity = int(request.POST.get('quantity'))
+    breed = request.POST['breed_selector']
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[service_id]['items_by_breed'][breed] = quantity
+        messages.success(request, f'We have updated {service.service_type} for\
+                                    {breed} to {quantity}')
+    else:
+        del quantity
+        messages.warning(request, f'0 is not a valid selection. Why not try\
+                                    the remove button? :)')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
