57f9dd7ef6fdf8bf359aaa1d1358d6c33789c72e
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 22:17:53 +0000

    hooked up links in urls

--- services/urls.py
+++ services/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
+    path('add/', views.add_service, name='add_service'),
+    path('edit/<slug:slug>', views.edit_service, name='edit_service'),
+    path('delete/<slug>', views.delete_service, name='delete_service'),
     path('<slug:slug>/', views.service_detail, name='service_detail'),
 ]
