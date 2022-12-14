09f21d18f74c8e1696944fd12bcef534c20fdc04
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 05:35:18 +0000

    add stripe javascript and css

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,31 @@
+/*
+    Core logic/paymenr flow for the comes from here:
+    https://stripe.com/docs/payments/quickstart
+
+    CSS from here:
+    https://stripe.com/docs/payments/elements
+
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey)
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#32325d',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {hidePostalCode: true, style: style});
+card.mount('#card-element');
+
