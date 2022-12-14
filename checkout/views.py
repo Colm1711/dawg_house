e53270120b574618bebb13bd22db65c328b9b96f
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 04:56:52 +0000

    add view to render form to template

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# Imports
 
-# Create your views here.
+# Django imports
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+# Internal imports
+from .forms import ServiceOderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.info(request, "There's nothing to checkout")
+        return redirect('services')
+
+    order_form = ServiceOderForm
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
