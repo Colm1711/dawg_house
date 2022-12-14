f72ed8fabb8d4cadb38e6a3e3e0194568732bb1a
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 03:46:25 +0000

    add signals for post save and post delete

---
+++ checkout/signals.py
@@ -0,0 +1,24 @@
+# Imports
+
+
+# Django imports
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+# Internal imports
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update the total on lineitem update/create
+    """
+    instance.serviceorder.update.total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update the total on lineitem update/create
+    """
+    instance.serviceorder.update.total()
