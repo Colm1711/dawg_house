89ba8ec122dba66df4723fa69909e14837bf08c2
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 22:15:48 +0000

    add on fail reload page to js func

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -77,4 +77,7 @@ form.addEventListener('submit', function(ev) {
             }
         }
     });
+    }).fail(function(){
+        //just reload the page, the error will be messages
+        location.reload();
 });
