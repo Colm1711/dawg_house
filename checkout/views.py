b8c24639edaabbd4e453e8d2c780147c6605e85b
Colm1711 <cosullivan1711@gmail.com>
2022-12-14 05:33:59 +0000

    add stripe to orderform

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
 # Imports
+import stripe
 
 # Django imports
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 # Internal imports
 from .forms import ServiceOderForm
 
@@ -15,8 +17,13 @@ def checkout(request):
 
     order_form = ServiceOderForm
     template = 'checkout/checkout.html'
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
