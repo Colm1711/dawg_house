624dac2210ff8925126f72c418df60567d1cc4af
Colm1711 <cosullivan1711@gmail.com>
2022-12-15 13:13:15 +0000

    add url link for terms and condition

--- home/urls.py
+++ home/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('privacypolicy/', views.privacypolicy, name='privacypolicy'),
+    path('termsconditions/', views.termsconditions, name='termsconditions'),
 ]
