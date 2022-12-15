d24d12400bd58c2ff47d428b5a5e78cc18ce2186
Colm1711 <cosullivan1711@gmail.com>
2022-12-15 19:58:53 +0000

    fixed email backend var

--- dawghouse/settings.py
+++ dawghouse/settings.py
@@ -200,7 +200,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'dawghouseireland@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
