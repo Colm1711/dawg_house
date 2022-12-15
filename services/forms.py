3674128cf876233d8e4812247bb90e9be3107e32
Colm1711 <cosullivan1711@gmail.com>
2022-12-15 19:13:56 +0000

    add review form

--- services/forms.py
+++ services/forms.py
@@ -3,12 +3,20 @@
 # Django imports
 from django import forms
 # Internal imports
-from .models import Service, Breed
+from .models import Service, Breed, Comment
 
 
 class ServiceForm(forms.ModelForm):
-    """Return Form to Super User to edit Service"""
+    """Return Form to Super User to add/edit Service"""
     class Meta:
         model = Service
         fields = "__all__"
         exclude = ('slug',)
+
+
+class ReviewForm(forms.ModelForm):
+    """Return Form to add a Service"""
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        exclude = ('created_on', 'is_approved',)
