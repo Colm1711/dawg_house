3d6df840f95bdd87aa7768a033c8cae7f2c36ee5
Colm1711 <cosullivan1711@gmail.com>
2022-12-16 03:28:35 +0000

    add httpresonse import

--- checkout/views.py
+++ checkout/views.py
@@ -3,7 +3,11 @@ import stripe
 import json
 
 # Django imports
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (render,
+                              redirect,
+                              reverse,
+                              get_object_or_404,
+                              HttpResponse)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
